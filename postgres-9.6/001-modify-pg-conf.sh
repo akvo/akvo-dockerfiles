51e1148bc50ba00920303466de8329aa4574d7be
Iván Perdomo <ivan@perdomo.me>
2018-10-16 18:17:40 +0200

    [#3] PostgreSQL 9.6 with self-signed certificate

---
+++ postgres-9.6/001-modify-pg-conf.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+
+PGDATA="${PGDATA:=/var/lib/postgresql/data}"
+
+# Enable SSL
+sed -i \
+    -e "s|^#ssl = off|ssl = on|" \
+    -e "s|^#ssl_cert_file =.*|ssl_cert_file = '${PGDATA}/server.crt'|" \
+    -e "s|^#ssl_key_file =.*|ssl_key_file = '${PGDATA}/server.key'|" \
+    "${PGDATA}/postgresql.conf"
+
+# Force SSL
+sed -i \
+    -e "/^host/d" \
+    -e "/^local/d" "${PGDATA}/pg_hba.conf"
+
+echo "hostssl  all  all  0.0.0.0/0  md5" >> "${PGDATA}/pg_hba.conf"
