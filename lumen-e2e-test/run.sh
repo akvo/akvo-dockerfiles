06b75bcd05e1ca8d0985635f3d03a3c8e1314db2
Iván Perdomo <ivan@perdomo.me>
2020-01-10 12:48:46 +0100

    [#22] Code clean up
    * We don't need `--` when calling `cypress run` as this is the way
      npm scripts use to pass parameters to programs:
    
      > Note the necessary --. It is needed to separate the params
      > passed to npm command itself and params passed to your script.
    
      https://stackoverflow.com/questions/11580961/sending-command-line-arguments-to-npm-script/14404223#14404223
    
    * Since we're not installing Cypress, nor using `npm run [...]` we
      don't need npm related files

--- lumen-e2e-test/run.sh
+++ lumen-e2e-test/run.sh
@@ -6,7 +6,7 @@ set -o nounset
 echo "Running Cypress against ${CYPRESS_LUMEN_URL}"
 
 if [[ -z "${CYPRESS_RECORD_KEY:=}" ]]; then
-    cypress run -- --project /app/e2e-test --browser chrome
+    cypress run --project /app/e2e-test --browser chrome
 else
-    cypress run -- --project /app/e2e-test --record --browser chrome
+    cypress run --project /app/e2e-test --record --browser chrome
 fi
