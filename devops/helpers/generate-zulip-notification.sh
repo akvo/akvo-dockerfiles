78aa47593983753300c95b099a8d32fa458a130b
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-05-12 15:27:58 +0200

    Check for Zulip API Key

--- devops/helpers/generate-zulip-notification.sh
+++ devops/helpers/generate-zulip-notification.sh
@@ -10,17 +10,18 @@ GITHUB_PROJECT=$6
 cat << EOF > notify.team.sh
 #!/usr/bin/env bash
 
-if [ -z "\${SLACK_CLI_TOKEN}" ]; then
-  echo "You need a env var SLACK_CLI_TOKEN with a Slack legacy token"
-  echo "Go to https://api.slack.com/custom-integrations/legacy-tokens to create one"
+if [ -z "\${ZULIP_CLI_TOKEN}" ]; then
+  echo "You need a env var ZULIP_CLI_TOKEN with a valid Zulip API key"
+  echo "See https://zulipchat.com/api/api-keys for instructions"
   echo "Create the env variable and rerun this script (\$0)"
+  echo "Environment variable format is YOUR_EMAIL:API_KEY"
   exit 1
 fi
 
 slack_txt=\$(git log --oneline $OLDER_GIT_VERSION..$NEWEST_GIT_VERSION | grep -v "Merge pull request" | grep -v "Merge branch" | cut -f 2- -d\  | sed 's/\[#\([0-9]*\)\]/#\1/' | tr \" \' | tr \& \ )
 
 curl -X POST https://akvo.zulipchat.com/api/v1/messages \
-    -u dan@akvo.org:wOdkvwntYhHf2Ge4ag5sZdOMIoqJ0t1y \
+    -u "\${ZULIP_CLI_TOKEN}" \
     -d "type=stream" \
     -d "to=rsr" \
     -d "topic=Releases" \
