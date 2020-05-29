f77ea35b27c3daa6795a4aa59145aa5b63bcd93a
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-05-29 09:58:58 +0200

    Permissions on for the Postgres user in Google Cloud SQL are not superuser, so there are bits and pieces that need to be Lumen user.
    Maybe there is an option to give that Postgres user those permissions.

--- lumen-db/z-init.sh
+++ lumen-db/z-init.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
-set -eu
+set -eux
 ON_ERROR_STOP=1
 export ON_ERROR_STOP
 
-# Provision
+POSTGRES_USER=postgres
+POSTGRES_DB=postgres
+POSTGRES_USER_PASSWORD="the password for the postgres user in Google Cloud SQL"
 
+psql_settings=("--username=${POSTGRES_USER}" "--host=localhost")
+export PGPASSWORD=$POSTGRES_USER_PASSWORD
+
+# Provision
 ## Create lumen role
-psql -v --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
+psql "${psql_settings[@]}" -v --dbname "${POSTGRES_DB}" <<-EOSQL
   CREATE USER lumen WITH CREATEDB PASSWORD 'password';
 EOSQL
 
 ## Create lumen dbs
-psql -v --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
+psql_settings=("--username=lumen" "--host=localhost")
+export PGPASSWORD="password"
+psql "${psql_settings[@]}" -v --dbname "${POSTGRES_DB}" <<-EOSQL
 CREATE DATABASE lumen
 WITH OWNER = lumen
      TEMPLATE = template0
@@ -35,12 +43,20 @@ EXTENSIONS="
   CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;"
 
 ## Create extensions for dbs
-psql -v --dbname lumen --command "${EXTENSIONS}"
+psql_settings=("--username=${POSTGRES_USER}" "--host=localhost")
+export PGPASSWORD=$POSTGRES_USER_PASSWORD
 
-psql -v --dbname test_lumen --command "${EXTENSIONS}"
+
+psql "${psql_settings[@]}" -v --dbname lumen --command "${EXTENSIONS}"
+
+psql "${psql_settings[@]}" -v --dbname test_lumen --command "${EXTENSIONS}"
+
+
+psql_settings=("--username=lumen" "--host=localhost")
+export PGPASSWORD="password"
 
 ## Create tenants dbs
-psql -v --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
+psql "${psql_settings[@]}" -v --dbname "${POSTGRES_DB}" <<-EOSQL
 
 CREATE DATABASE lumen_tenant_1
 WITH OWNER = lumen
@@ -71,11 +87,14 @@ WITH OWNER = lumen
      LC_CTYPE = 'en_US.UTF-8';
 EOSQL
 
+psql_settings=("--username=${POSTGRES_USER}" "--host=localhost")
+export PGPASSWORD=$POSTGRES_USER_PASSWORD
+
 ## Create extensions for dbs
-psql --dbname lumen_tenant_1 --command "${EXTENSIONS}"
-psql --dbname lumen_tenant_2 --command "${EXTENSIONS}"
-psql --dbname test_lumen_tenant_1 --command "${EXTENSIONS}"
-psql --dbname test_lumen_tenant_2 --command "${EXTENSIONS}"
+psql "${psql_settings[@]}" --dbname lumen_tenant_1 --command "${EXTENSIONS}"
+psql "${psql_settings[@]}" --dbname lumen_tenant_2 --command "${EXTENSIONS}"
+psql "${psql_settings[@]}" --dbname test_lumen_tenant_1 --command "${EXTENSIONS}"
+psql "${psql_settings[@]}" --dbname test_lumen_tenant_2 --command "${EXTENSIONS}"
 
 echo "----------"
 echo "Done!"
