9df443914ee2e29986cd1e7986f6e9e8e7c48fb5
Emmanuel Mulo <emmanuel@akvo.org>
2021-05-19 13:24:49 +0200

    [#62] Change source prod instance

--- devops/flow/flip-production-traffic.sh
+++ devops/flow/flip-production-traffic.sh
@@ -14,7 +14,7 @@ function read_version () {
 
 GITHUB_PROJECT=akvo-flow
 
-PROD_CLUSTER="akvoflow-23" # WHH instance
+PROD_CLUSTER="akvoflow-194"
 
 read_version $PROD_CLUSTER
 PROD_LIVE_VERSION=$VERSION
