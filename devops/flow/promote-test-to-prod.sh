4430fc28537b6cd3d38b690f296efb7e3f5767d7
zuhdil <zuhdil@gmail.com>
2022-11-16 13:51:54 +0700

    [#69] Update zulip stream for Akvo Flow release notification

--- devops/flow/promote-test-to-prod.sh
+++ devops/flow/promote-test-to-prod.sh
@@ -74,7 +74,7 @@ else
    PROMOTION_REASON="REGULAR_RELEASE"
 fi
 
-generate-zulip-notification.sh "${NEWEST_VERSION_IN_PROD}" "${TEST_LIVE_VERSION}" "Promoting ${GITHUB_PROJECT} to dark production (aka https://uat1.akvoflow.org)" "good" "wrap_slack" "$GITHUB_PROJECT" "K2 Engine"
+generate-zulip-notification.sh "${NEWEST_VERSION_IN_PROD}" "${TEST_LIVE_VERSION}" "Promoting ${GITHUB_PROJECT} to dark production (aka https://uat1.akvoflow.org)" "good" "wrap_slack" "$GITHUB_PROJECT" "Flumen"
 
 log "To deploy, run: "
 echo "----------------------------------------------"
